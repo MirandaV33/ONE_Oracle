608006d5c201be09057c17a0029bb90752f96143
MirandaV33 <mimiviceconte@gmail.com>
2025-08-07 12:43:55 -0300

    Challenge results

--- Logica de programacion/Challenge 1/app.js
+++ Logica de programacion/Challenge 1/app.js
@@ -14,3 +14,46 @@ un nombre de la lista y se mostrará en la página.
 
 */
 
+let ListaAmigos= [ ]; 
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre); 
+    if (nombre == ''){
+        alert('Ingrese un nombre válido'); 
+    } 
+    else {
+        ListaAmigos.push(nombre);
+    }
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML += `<li>${nombre}</li>`;
+    document.getElementById('amigo').value = '';
+    // Limpio el campo de resultado para comenzar otra vez
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function sortearAmigo() {
+    if (ListaAmigos.length === 0) {
+        alert('No hay amigos para sortear');
+        return;
+    }
+    else{
+        let nombresorteado= Math.floor(Math.random() * ListaAmigos.length); 
+        let nombre = ListaAmigos[nombresorteado];
+
+        let listaHTML = document.getElementById('resultado');
+        listaHTML.innerHTML = `<li>${nombre}</li>`;
+
+        //Limpiamos la lista para comenzar otra vez
+        ListaAmigos = [];
+        document.getElementById('listaAmigos').innerHTML = '';
+        document.getElementById('amigo').value = ''; 
+    }
+}
+
+document.getElementById('amigo').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        agregarAmigo();
+    }
+});
